40d08eb1a5a22ec3faa3f3a43b49bbf22b158428
felipe <enggazolla@gmail.com>
2024-07-30 14:36:38 -0300

    first commit

---
+++ src/routes/user.routes.js
@@ -0,0 +1,19 @@
+const { Router, request } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
+
+const UsersController = require("../controllers/UserController")
+const UserAvatarController = require("../controllers/UserAvatarController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
+const userRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
+
+const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
+
+userRoutes.post("/", usersController.create)
+userRoutes.put("/", ensureAuthenticated, usersController.update)
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
+
+module.exports = userRoutes
